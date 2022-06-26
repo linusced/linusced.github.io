d845186255886558c15836e28f4fa869d426c46c
= <=>
2022-06-26 15:11:25 +0200

    commit

--- assets/js/images.js
+++ assets/js/images.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
     const bgImage = document.querySelector(".bg-image");
 
     window.addEventListener("scroll", () => {
-        if (window.scrollY >= bottom)
+        if (window.scrollY >= window.innerHeight)
             bgImage.classList.add("absolute");
         else
             bgImage.classList.remove("absolute");
